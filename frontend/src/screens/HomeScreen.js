df3c210478b29af87abc2b190fae1c54adae4857
VanVDC <vanbvy@gmail.com>
2020-12-08 19:33:37 -0500

    first commit

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import {Row, Col} from 'react-bootstrap'
+import Product  from '../components/Product'
+
+const HomeScreen = () => {
+  const [products, setProducts] =useState([])
+
+  useEffect(()=>{
+    const fetchProducts = async ()=>{
+      const {data} = await axios.get('/api/products')
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) =>
+         ( <Col key={product._id}sm={12} md={6} lg={4} xl={3}>
+          <Product product={product}/>
+          </Col>))}
+      </Row>
+      
+    </div>
+  )
+}
+
+export default HomeScreen
