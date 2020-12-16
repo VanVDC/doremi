8f820a058dffc7d121ec7bb2656fb11c379a3d50
VanVDC <vanbvy@gmail.com>
2020-12-16 11:54:53 -0500

    added user reducer, constants, actions, store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { userLoginReducer } from './reducers/userReducers'
 import {
   studentListReducer,
   studentDetailsReducer,
@@ -15,12 +16,22 @@ const reducer = combineReducers({
   studentDelete: studentDeleteReducer,
   studentCreate: studentCreateReducer,
   studentUpdate: studentUpdateReducer,
+  userLogin: userLoginReducer,
 })
+
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+}
 
 const middleware = [thunk]
 
 const store = createStore(
   reducer,
+  initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 )
 
