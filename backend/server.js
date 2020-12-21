fd1e2ea5df12ad28645297d3437916b60065b731
VanVDC <vanbvy@gmail.com>
2020-12-21 18:57:05 -0500

    set server ready to deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
@@ -11,10 +12,26 @@ connectDB()
 
 const app = express()
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.use(express.json())
 
 app.use('/api/students', studentRoutes)
 app.use('/api/users', userRoutes)
+
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is Running...')
+  })
+}
 
 //check error
 app.use(notFound)
