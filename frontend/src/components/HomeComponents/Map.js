43441663c427637eeec10cf6a9a7ce668de79ccc
VanVDC <vanbvy@gmail.com>
2020-12-21 20:00:46 -0500

    added iframe map

--- frontend/src/components/HomeComponents/Map.js
+++ frontend/src/components/HomeComponents/Map.js
@@ -7,11 +7,8 @@ const Map = () => {
         title='mapMarker'
         src='https://www.google.com/maps/d/embed?mid=1yrofSsxqpcT7Q84GTCfj3Pf79MrM8OYr'
         width='100%'
-        height='450'
-        frameBorder='0'
-        style={{ border: 0, marginBottom: '-7px' }}
-        allowFullScreen
-      />
+        height='480'
+      ></iframe>
     </div>
   )
 }
