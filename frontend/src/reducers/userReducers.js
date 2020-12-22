c3d2de4f0e62b708d74b450413e554bb08f4c5b0
VanVDC <vanbvy@gmail.com>
2020-12-22 18:14:32 -0500

    fixed user issue

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -64,7 +64,7 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     case USER_DETAILS_RESET:
-      return { USER: {} }
+      return { user: {} }
     default:
       return state
   }
