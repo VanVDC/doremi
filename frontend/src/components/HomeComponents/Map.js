4f0d49bbdbe30afc3daf7633eb8fffc1445452b7
VanVDC <vanbvy@gmail.com>
2020-12-21 20:14:22 -0500

    fixed google map error

--- frontend/src/components/HomeComponents/Map.js
+++ frontend/src/components/HomeComponents/Map.js
@@ -8,6 +8,7 @@ const Map = () => {
         src='https://www.google.com/maps/d/embed?mid=1yrofSsxqpcT7Q84GTCfj3Pf79MrM8OYr'
         width='100%'
         height='480'
+        accesscontrolalloworigin='$0'
       ></iframe>
     </div>
   )
