0d4093e266f9975373c83dfde6f599ac2c67120a
VanVDC <vanbvy@gmail.com>
2020-12-21 20:24:25 -0500

    fixed map error

--- frontend/src/components/HomeComponents/Map.js
+++ frontend/src/components/HomeComponents/Map.js
@@ -5,7 +5,7 @@ const Map = () => {
     <div>
       <iframe
         title='map'
-        src='https://www.google.com/maps/d/embed?mid=1yrofSsxqpcT7Q84GTCfj3Pf79MrM8OYr'
+        src='http://www.google.com/maps/d/embed?mid=1yrofSsxqpcT7Q84GTCfj3Pf79MrM8OYr'
         width='100%'
         height='480'
         accesscontrolalloworigin='$0'
