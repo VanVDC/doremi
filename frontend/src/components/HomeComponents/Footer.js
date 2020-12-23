9cd306800f37c7f3c4de2568c5658af0241a0e64
VanVDC <vanbvy@gmail.com>
2020-12-22 19:23:02 -0500

    added the date to footer

--- frontend/src/components/HomeComponents/Footer.js
+++ frontend/src/components/HomeComponents/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
+  const currentDate = new Date()
   return (
     <div className='bottom-footer pt-3 pb-3 text-center'>
-      <small>Copyright &copy; Doremi</small>
+      <small>
+        Copyright &copy; {currentDate.getFullYear()} Doremi Music Lessons
+      </small>
     </div>
   )
 }
