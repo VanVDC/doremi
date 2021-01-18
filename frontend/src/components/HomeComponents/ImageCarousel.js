5fe86fb8caeb49fe169d6f8af81fd8ef0dfe108b
VanVDC <vanbvy@gmail.com>
2021-01-18 16:05:34 -0500

    remove facebook image

--- frontend/src/components/HomeComponents/ImageCarousel.js
+++ frontend/src/components/HomeComponents/ImageCarousel.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Carousel, Image } from 'react-bootstrap'
 import { imageData } from '../../data/data'
-
 const ImageCarousel = () => {
   return (
     <Carousel pause='hover' className='bg-dark text-center'>
       {imageData.map((image, i) => (
         <Carousel.Item key={i}>
-          <Image className=' w-50' src={image} fluid />
+          <Image className='w-50' src={image} fluid />
         </Carousel.Item>
       ))}
     </Carousel>
