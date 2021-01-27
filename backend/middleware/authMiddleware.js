c9fe2b64cbbdfb2d886bd3e684fbc0e3c601ab93
VanVDC <vanbvy@gmail.com>
2021-01-26 20:17:13 -0500

    edited authmiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,41 +1,39 @@
-import jwt from 'jsonwebtoken'
-import asyncHandler from 'express-async-handler'
-import User from '../models/userModel.js'
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token = req.headers.authorization
+  let token = req.headers.authorization;
 
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      token = req.headers.authorization.split(' ')[1]
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
 
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-      req.user = await User.findById(decoded.id).select('-password')
-
-      next()
+      next();
     } catch (error) {
-      console.error(error)
-      res.status(401)
-      throw new Error('Not authoried, token failed')
+      console.error(error);
+      res.status(401);
+      throw new Error('Not authoried, token failed');
     }
   }
 
   if (!token) {
-    res.status(401)
-    throw new Error('Not authorized no token')
+    res.status(401);
+    throw new Error('Not authorized no token');
   }
-})
+});
 const admin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
-    next()
+    next();
   } else {
-    res.status(401)
-    throw new Error('Not authorized as admin')
+    res.status(401);
+    throw new Error('Not authorized as admin');
   }
-}
+};
 
-export { protect, admin }
+export { protect, admin };
